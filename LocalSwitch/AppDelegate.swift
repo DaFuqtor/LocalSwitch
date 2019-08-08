836f9f762cecf779b58766b1c7d40e06e523852e
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-08 18:24:40 +0300

    Switch using Spotlight
    Opening LocalSwitch when it's already running switches server state

--- LocalSwitch/AppDelegate.swift
+++ LocalSwitch/AppDelegate.swift
@@ -2,4 +2,15 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication,
+                                       hasVisibleWindows flag: Bool) -> Bool
+    {
+        if (servCheck().isEmpty) {
+            shell("sudo apachectl graceful")
+        } else {
+            shell("sudo apachectl graceful-stop")
+        }
+        return true;
+    }
 }
